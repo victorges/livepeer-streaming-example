5cb493cc18e2ba9d3523656487120df71140b8e1
danisharora099 <danisharora099@gmail.com>
2021-09-20 19:44:18 +0530

    init

---
+++ constants.js
@@ -0,0 +1,58 @@
+const URL = "https://www.youtube.com/watch?v=OrrkuUlFfOQ";
+
+const testStreamKey = "df18-hte7-u0lv-fey3";
+
+const puppeteerArgs = (display) => {
+  [
+    "--enable-usermedia-screen-capturing",
+    "--allow-http-screen-capture",
+    "--no-sandbox",
+    "--allow-insecure-localhost",
+    "--autoplay-policy=no-user-gesture-required",
+    "--start-fullscreen",
+    "--window-size=1920,1080",
+    `--display=${display}`,
+  ];
+};
+
+const ffmpegArgs = (streamKey) => {
+  return [
+    "-i",
+    "-",
+
+    // video codec config: low latency, adaptive bitrate
+    "-c:v",
+    "libx264",
+    "-preset",
+    "veryfast",
+    "-tune",
+    "zerolatency",
+
+    // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
+    "-c:a",
+    "aac",
+    "-ar",
+    "44100",
+    "-b:a",
+    "64k",
+
+    //force to overwrite
+    "-y",
+
+    // used for audio sync
+    "-use_wallclock_as_timestamps",
+    "1",
+    "-async",
+    "1",
+
+    //'-filter_complex', 'aresample=44100', // resample audio to 44100Hz, needed if input is not 44100
+    //'-strict', 'experimental',
+    "-bufsize",
+    "1000",
+    "-f",
+    "flv",
+    `rtmp://rtmp.livepeer.com/live/${streamKey}`,
+  ];
+};
+
+export { URL, puppeteerArgs, ffmpegArgs, testStreamKey };
---
+++ index.js
@@ -0,0 +1,51 @@
+import child_process from "child_process";
+import { getStream, launch } from "puppeteer-stream";
+import { ffmpegArgs, puppeteerArgs, testStreamKey, URL } from "./constants.js";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const Xvfb = require("xvfb");
+
+const xvfb = new Xvfb({
+  silent: true,
+  xvfb_args: ["-screen", "0", "1920x1080x24", "-ac"],
+});
+
+async function main() {
+  try {
+    xvfb.startSync((err) => {
+      console.error("Error while starting XVFB - ", err);
+    });
+
+    const browser = await launch({
+      defaultViewport: null,
+      headless: false,
+      args: puppeteerArgs(xvfb._display),
+    });
+
+    const page = await browser.newPage();
+
+    await page.evaluateOnNewDocument(async () => {
+      console.info(`Recording bot going to - ${URL}`);
+
+      await page.goto(URL, {
+        waitUntil: "networkidle2",
+        timeout: 0,
+      });
+    });
+
+    const stream = await getStream(page, { audio: true, video: true });
+
+    const ffmpeg = child_process.spawn("ffmpeg", ffmpegArgs(testStreamKey));
+
+    // stream.pipe(ffmpeg.stdin);
+
+    stream.on("data", (data) => {
+      console.info(Date.now(), " - received data");
+      ffmpeg.stdin.write(data);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
